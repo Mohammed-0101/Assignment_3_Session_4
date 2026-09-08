19d692634b5fd452445a4a9323dc59cecfc79794
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-08 04:21:09 +0300

    Q12

--- Assignment_3_Session_4/Program.cs
+++ Assignment_3_Session_4/Program.cs
@@ -10,12 +10,26 @@ namespace Assignment_3_Session_4
     {
         static void Main(string[] args)
         {
-            #region Declare int copies = 100;. Box it into an object variable, then unbox it back into a new int variable, and print both.
+            #region 11.Declare int copies = 100;. Box it into an object variable, then unbox it back into a new int variable, and print both.
             int copies = 100;
             object boxedCopies = copies;
             int unboxedCopies = (int)boxedCopies;
             Console.WriteLine(copies);
             Console.WriteLine(unboxedCopies);
+            #endregion
+
+            #region 12.Given int pages = 464;, write an if / else statement that prints "Long Book" if pages is greater than 300, otherwise prints "Short Book".
+
+            int pages = 464;
+
+            if (pages > 300)
+            {
+                Console.WriteLine("Long Book");
+            }
+            else
+            {
+                Console.WriteLine("Short Book");
+            }
             #endregion
 
         }
