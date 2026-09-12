f49387f56e5ac6cfe8940a77de9686987a370fc4
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-12 14:28:31 +0300

    Sheet 4 Q4

--- Assignment_3_Session_4/Program.cs
+++ Assignment_3_Session_4/Program.cs
@@ -15,6 +15,11 @@ namespace Assignment_3_Session_4
             Console.WriteLine("Welcome to the Library!");
         }
 
+        static void PrintBookTitle(string title)
+        {
+            Console.WriteLine("Book title: " + title);
+        }
+
         static void Main(string[] args)
         {
             // Sheet 4
@@ -42,7 +47,9 @@ namespace Assignment_3_Session_4
             PrintWelcomeMessage();
             #endregion
 
-
+            #region 4-Write a method PrintBookTitle(string title) that prints "Book title: " + title. Call it with "Clean Code".
+            PrintBookTitle("Clean Code");
+            #endregion
 
         }
     }
