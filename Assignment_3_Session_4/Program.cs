3502efd9e676873ba5df8e0edbf9efa127d5ccaa
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-12 14:22:39 +0300

    Sheet 4 Q2

--- Assignment_3_Session_4/Program.cs
+++ Assignment_3_Session_4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,19 @@ namespace Assignment_3_Session_4
             double[] prices1 = { 25.5, 40.0, 33.75 };
 
             Console.WriteLine("Question 1:");
-            Console.WriteLine(prices1[1]);   
+            Console.WriteLine(prices1[1]);
+            #endregion
+
+            #region 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+
+            int[,] shelfCopies =
+            {
+                { 3, 5 },
+                { 1, 4 }
+            };
+
+            Console.WriteLine("\nQuestion 2:");
+            Console.WriteLine(shelfCopies[1, 0]);
             #endregion
         }
     }
